5d8078a833b9b1950db0688e8a4519686f45a047
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-13 23:24:30 +0700

    feat: change type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,10 @@
-type AwesomeOptions = {
-  customError?: (err: Error) => Error;
-};
+declare module "dookdiks-utils" {
+  type AwesomeOptions = {
+    customError?: (err: Error) => Error;
+  };
 
-type AwesomeSuccess<T> = { data: T; error: null };
-type AwesomeError<T = Error> = { data: null; error: T };
-type ReturnAsyncAwesome<T> = Promise<AwesomeSuccess<T> | AwesomeError>
-type ReturnAwesome<T> = AwesomeSuccess<T> | AwesomeError
+  type AwesomeSuccess<T> = { data: T; error: null };
+  type AwesomeError<T = Error> = { data: null; error: T };
+  type ReturnAsyncAwesome<T> = Promise<AwesomeSuccess<T> | AwesomeError>
+  type ReturnAwesome<T> = AwesomeSuccess<T> | AwesomeError
+}
