6a825c938e1337ec620b41391a3669b9b3df8281
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2023-11-14 11:56:41 +0700

    feat: update type

--- src/handler/awesome.ts
+++ src/handler/awesome.ts
@@ -1,24 +1,3 @@
-type AwesomeOptions = {
-  customError?: (err: Error) => Error;
-};
-
-type AwesomeAsync = <T>(
-  func: () => Promise<T>,
-  options?: AwesomeOptions
-) => Promise<AwesomeResult<T>>;
-
-type AwesomeSync = <T>(
-  func: () => T,
-  options?: AwesomeOptions
-) => AwesomeResult<T>
-
-type AwesomeResult<T> = { data: T | null; error: Error | null };
-
-type AwesomeFunction = {
-  async: AwesomeAsync
-  sync: AwesomeSync
-};
-
 export const awesomeAsync: AwesomeAsync = async (func, options) => {
   try {
     const data = await func();
