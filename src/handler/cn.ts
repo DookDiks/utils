d12d72cde9fdc6a8981235badec1c9a7edcab8c8
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-01-04 22:03:56 +0700

    feat: change comment 2

--- src/handler/cn.ts
+++ src/handler/cn.ts
@@ -4,6 +4,7 @@ import { twMerge } from 'tailwind-merge';
 /**
  * A utility function for creating composite class names using clsx and tailwind-merge.
  *
+ *
  * @param {...ClassValue} classes - Class names or conditional classes to be merged.
  * @returns {string} - The merged class names.
  */
